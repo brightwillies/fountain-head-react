26abaacdb7efea19b73305b89451f67e4938bfcc
brightwillies <bwboakye2@gmail.com>
2026-04-03 08:54:20 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Worcester Academy | Co-ed Day And Boarding School For Grades 6 To 12 And Postgraduates",
-  description: "Worcester Academy is an independent co-educational day and boarding school for Middle School, Upper School, and Postgraduate students set on 71 acres in the heart of Worcester, Massachusetts, New England's second largest city.",
+  title: "Fountainhead Christian School ",
+  description: "Fountainhead Christian School",
   icons: {
     icon: "/favicon.ico",
   },
